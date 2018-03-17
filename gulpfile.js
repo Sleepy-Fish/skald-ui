2cf2331a535eed1d94693c1b24e65b3f68fa2cd2
James Harrington <JRileyHarrington@gmail.com>
2018-03-17 14:55:30 -0400

    setup circle

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
 var gulp = require('gulp');
+var sequence = require('run-sequence');
+var clean = require('gulp-clean');
 
-gulp.task('build', function(){
+gulp.task('clean', function () {
+    return gulp.src('./dist', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('html', function(){
     return gulp.src('./index.html')
-        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('./dist/build'))
 })
+
+gulp.task('test', function () {
+    gulp.src('./test/*')
+        .pipe(gulp.dest('./dist/test'))
+});
+
+gulp.task('build', function (cb) {
+    sequence('clean', ['html', 'test'], cb);
+});
+
+gulp.task('dev-build', function (cb) {
+    sequence('clean', ['html', 'test'], cb);
+});
+
+gulp.task('default', function (cb) {
+    sequence('dev-build', cb)
+});
